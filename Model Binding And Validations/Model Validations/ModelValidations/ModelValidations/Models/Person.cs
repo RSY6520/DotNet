4e6b63de0ca75a8decf9e472d8ff4002e72388b0
RAMAKANT SINGH YADAV <rsy6520@gmail.com>
2025-02-07 03:24:17 +0530

    model validations

---
+++ Model Binding And Validations/Model Validations/ModelValidations/ModelValidations/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidations.Models
+{
+    public class Person
+    {
+        [Required]
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public double? Price { get; set; }
+    }
+}
