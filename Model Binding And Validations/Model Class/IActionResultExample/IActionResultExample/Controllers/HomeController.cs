66088ccb4335068d5c0b496c5cc93770b27412df
RAMAKANT SINGH YADAV <rsy6520@gmail.com>
2025-02-07 02:44:31 +0530

    Model Class with binding

---
+++ Model Binding And Validations/Model Class/IActionResultExample/IActionResultExample/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using IActionResultExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("bookstore/{bookid?}/{isloggedin?}")]
+        public IActionResult Index([FromQuery] int? bookid, [FromRoute] bool? isloggedin, Book book)
+        {
+            if (bookid.HasValue == false)
+            {
+                return BadRequest("Bookid not supplied or empty");
+            }
+            //Book id can't be less than or equal to 0
+            if (bookid <= 0)
+            {
+                return BadRequest("Book id can't be less than or equal to 0");
+            }
+
+            //Book id should be between 1 to 1000
+            if (bookid <= 0)
+            {
+                return BadRequest("Book id can't be less than or equal to zero");
+            }
+            if (bookid > 1000)
+            {
+                return NotFound("Book id can't be greater than 1000");
+            }
+
+            //isloggedin should be true
+            if (isloggedin == false)
+            {
+                return StatusCode(401);
+            }
+
+            return Content($"Book id: {bookid}, Book: {book}", "text/plain");
+        }
+    }
+}
---
+++ Model Binding And Validations/Model Class/IActionResultExample/IActionResultExample/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultExample.Models
+{
+    public class Book
+    {
+        [FromQuery]
+        public int? Id { get; set; }
+
+        public string? Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"Book object, BookId: {Id} Author: {Author}";
+        }
+    }
+}
