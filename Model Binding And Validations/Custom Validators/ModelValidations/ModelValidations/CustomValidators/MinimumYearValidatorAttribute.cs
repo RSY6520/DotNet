34115deb68e47c8a38dbb9420b0000e75218973c
RAMAKANT SINGH YADAV <rsy6520@gmail.com>
2025-02-07 08:46:50 +0530

    Custom Validation

---
+++ Model Binding And Validations/Custom Validators/ModelValidations/ModelValidations/CustomValidators/MinimumYearValidatorAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidations.CustomValidators
+{
+    public class MinimumYearValidatorAttribute: ValidationAttribute
+    {
+        public int MinimumYear { get; set; } = 2000;
+
+        public string DefaultErrorMessage => $"The year must be greater than {MinimumYear}";
+        public MinimumYearValidatorAttribute() {
+        
+        }
+
+        public MinimumYearValidatorAttribute(int minimumYear)
+        {
+            MinimumYear = minimumYear;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                DateTime date = (DateTime)value;
+                if (date.Year < MinimumYear)
+                {
+                    return new ValidationResult(string.Format(ErrorMessage ?? DefaultErrorMessage, MinimumYear));
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
