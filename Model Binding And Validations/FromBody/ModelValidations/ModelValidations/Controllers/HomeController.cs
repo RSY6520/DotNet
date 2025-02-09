217e8ccf3af3eedc9abe1760f5c465e9d88e4839
RAMAKANT SINGH YADAV <rsy6520@gmail.com>
2025-02-09 10:23:45 +0530

    from body

---
+++ Model Binding And Validations/FromBody/ModelValidations/ModelValidations/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidations.Models;
+
+namespace ModelValidationsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("register")]
+        [Bind(nameof(Person.PersonName), nameof(Person.Email), nameof(Person.Phone), nameof(Person.Password), nameof(Person.ConfirmPassword), nameof(Person.Price), nameof(Person.DateOfBirth), nameof(Person.FromDate), nameof(Person.ToDate), nameof(Person.Age)]
+        public IActionResult Index([FromBody]Person person)
+        {
+            if(!ModelState.IsValid)
+            {
+                string errors = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage));
+                return BadRequest(errors);
+            }
+            return Content($"{person}");
+        }
+    }
+}
