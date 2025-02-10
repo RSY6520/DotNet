3aee103f5da4082188f297827992f2452e234991
RAMAKANT SINGH YADAV <rsy6520@gmail.com>
2025-02-10 09:59:41 +0530

    strongly typed view components, view components with parameters, viewcomponentresult

--- View Components/ViewComponents/ViewComponents/Controllers/HomeController.cs
+++ View Components/ViewComponents/ViewComponents/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponents.Models;
 
 namespace ViewComponents.Controllers
 {
@@ -14,6 +15,22 @@ namespace ViewComponents.Controllers
         public IActionResult About()
         {
             return View();
+        }
+
+        [Route("friends-list")]
+        public IActionResult LoadFriendsList()
+        {
+            PersonGridModel friends = new PersonGridModel()
+            {
+                GridTitle = "Friends",
+                Persons = new List<Person>()
+                {
+                    new Person() { PersonName = "Mia", JobTitle = "Developer" },
+                    new Person() { PersonName = "Emma", JobTitle = "UI Designer" },
+                    new Person() { PersonName = "Avva", JobTitle = "QA" }
+                }
+            };
+            return ViewComponent("Grid", new { grid = friends});
         }
     }
 }
--- View Components/ViewComponents/ViewComponents/ViewComponents/GridViewComponent.cs
+++ View Components/ViewComponents/ViewComponents/ViewComponents/GridViewComponent.cs
@@ -5,19 +5,24 @@ namespace ViewComponents.ViewComponents
 {
     public class GridViewComponent: ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync()
+        //public async Task<IViewComponentResult> InvokeAsync()
+        //{
+        //    PersonGridModel personGridModel = new PersonGridModel()
+        //    {
+        //        GridTitle = "Persons List",
+        //        Persons = new List<Person>() {
+        //          new Person() { PersonName = "John", JobTitle = "Manager" },
+        //          new Person() { PersonName = "Jones", JobTitle = "Asst. Manager" },
+        //          new Person() { PersonName = "William", JobTitle = "Clerk" },
+        //        }
+        //    };
+        //    //ViewData["Grid"] = model;
+        //    return View("Sample", personGridModel);
+        //}
+        public async Task<IViewComponentResult> InvokeAsync(PersonGridModel grid)
         {
-            PersonGridModel personGridModel = new PersonGridModel()
-            {
-                GridTitle = "Persons List",
-                Persons = new List<Person>() {
-                  new Person() { PersonName = "John", JobTitle = "Manager" },
-                  new Person() { PersonName = "Jones", JobTitle = "Asst. Manager" },
-                  new Person() { PersonName = "William", JobTitle = "Clerk" },
-                }
-            };
-            //ViewData["Grid"] = model;
-            return View("Sample", personGridModel);
+            
+            return View("Sample", grid);
         }
     }
 }
